32cd784929b9f89a748459900726844588d3e395
srj-17 <saugatrijal17@gmail.com>
2024-06-22 08:35:47 +0545

    Add feature for drawing with opacity
    Each time a cursor moves over a pixel, opacity is increased.
    This adds realism to the drawing.

--- script.js
+++ script.js
@@ -9,26 +9,33 @@ function createSquareGrid(size) {
         for (let j = 0; j < size; j++) {
             const pixel = document.createElement('div');
             pixel.classList.toggle('pixel');
-            pixel.style.cssText = 'background-color : grey; width: 15px; height: 15px; flex: auto;';
+            pixel.style.cssText = 'width: 15px; height: 15px; flex: auto;';
             row.appendChild(pixel);
         }   
         container.appendChild(row);
     }
 }
 
-function colorPixel(pixel) {
+function colorPixel(pixel, count) {
     let red = Math.floor(Math.random() * 255);
     let green = Math.floor(Math.random() * 255);
     let blue = Math.floor(Math.random() * 255);
-    pixel.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    let opacity;
+    if (count < 10) {
+        console.log(count);
+        opacity = count * 0.1;
+    } 
+    pixel.style.backgroundColor = `rgb(${red}, ${green}, ${blue}, ${opacity})`;
 }
 
 function draw() {
     const pixelsNodeList = document.querySelectorAll('.pixel');
     const pixels = Array.from(pixelsNodeList);
     pixels.forEach(pixel => {
+        let count = 0;
         pixel.addEventListener('mouseenter', () => {
-            colorPixel(pixel);
+            colorPixel(pixel, count);
+            count++;
         })        
     });
 }
